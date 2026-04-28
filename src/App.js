15e1dc20c11dce2225b092da8ca4f106100ce1f0
Your Name <brajeshpatel@gmail.com>
2026-04-28 12:05:38 +0530

    Changes in app.js file only to check the updats in Frontend and e2e test file to check the updates in e2e test cases. This is to check the updates in both files and to check the build status in Jenkins.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins with Docker on Udemy
         </a>
       </header>
       <p>
